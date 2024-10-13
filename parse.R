4995a3f5f14750eabf518db829714fc485bf5322
Tuija <tuija.sonkkila@aalto.fi>
2024-10-13 15:33:44 +0300

    Initial commit

---
+++ parse.R
@@ -0,0 +1,95 @@
+library(readxl)
+library(tidyverse)
+library(pxweb)
+library(geofi)
+library(sf)
+library(patchwork)
+
+#---------------------------------
+# Data licensed from Tilastokeskus. 
+# Number of people by municipality 
+# in 2023, born in Laitila
+#---------------------------------
+data <- read_excel("24PIENEP_418.xlsx", skip = 2)
+laitila <- data %>% 
+  filter(Kuntakoodi != "SSS") %>% 
+  rename(laitilasta = `2023`)
+
+#---------------------------------------------
+# Open data on municipalities, population 2023
+#
+# "https://statfin.stat.fi/PXWeb/api/v1/fi/StatFin/vaerak/statfin_vaerak_pxt_11re.px"
+#
+#---------------------------------------------
+
+d <- pxweb_interactive("https://statfin.stat.fi/PXWeb/api/v1/fi")
+data_all <- d$data
+
+data_all <- data_all %>% 
+  rename(Kuntanimi = Alue,
+         kuntalaisia = `Väestö 31.12.`) %>% 
+  mutate(Kuntanimi = ifelse(Kuntanimi == "Maarianhamina - Mariehamn", "Maarianhamina", Kuntanimi)) %>% 
+  select(Kuntanimi, kuntalaisia)
+
+saveRDS(data_all, "vaesto_kunnittain_2023.RDS")
+
+data_all_l <- left_join(data_all, laitila)
+
+share <- data_all_l %>% 
+  mutate(laitilasta = as.numeric(as.character(laitilasta)),
+         kuntalaisia = as.numeric(as.character(kuntalaisia)),
+         laitilalaistenosuus = round(100 * (laitilasta / kuntalaisia), digits = 2),
+         Kuntakoodi = gsub("^0+", "", Kuntakoodi)) %>% 
+  filter(Kuntanimi != "KOKO MAA")
+
+#--------------------------
+# Municipalities by geofi
+#--------------------------
+d1 <- get_municipalities(year = 2023)
+
+saveRDS(d1, "kunnat.RDS")
+
+kunnat_geom <- d1 %>% 
+  select(kunta, nimi, geom) %>% 
+  rename(Kuntakoodi = kunta) %>% 
+  mutate(Kuntakoodi = as.character(Kuntakoodi))
+
+share_geom <- kunnat_geom %>%
+  right_join(share) %>% 
+  filter(!is.na(laitilasta))
+
+share_geom$Osuus <- cut(
+  share_geom$laitilalaistenosuus,
+  breaks = c(0.0, 0.50, 1, 10, 60), 
+  include.lowest = TRUE,
+  dig.lab=4
+)
+share_geom$Lkm <- cut(
+  share_geom$laitilasta,
+  breaks = c(1, 10, 50, 500, 1000, 5000),
+  include.lowest = TRUE,
+  dig.lab = 4
+)
+
+saveRDS(share_geom, "laitila_muualla.RDS")
+
+m1 <- ggplot() +
+  geom_sf(data = d1) +
+  geom_sf(data = share_geom, 
+       aes(fill = Osuus)) +
+  scale_fill_viridis_d(option = "inferno", direction = -1) +
+  guides(fill = guide_legend(title = "Osuus väestöstä %")) +
+  theme_minimal()
+
+m2 <- ggplot() +
+  geom_sf(data = polygon) +
+  geom_sf(data = share_geom, 
+          aes(fill = Lkm)) +
+  scale_fill_viridis_d(option = "inferno", direction = -1) +
+  guides(fill = guide_legend(title = "Lukumäärä")) +
+  theme_minimal()
+
+(m2 | m1) + 
+  plot_layout(nrow = 1) +
+  plot_annotation(title = "Laitilassa syntyneiden asuinkunta vuonna 2023")
+
